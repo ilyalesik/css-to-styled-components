4f001429bff6e5b50eca08e413299813e54efff3
Ilia Lesik <ilialesik@gmail.com>
2017-10-18 12:49:51 +0300

    converter

--- converter.js
+++ converter.js
@@ -1,6 +1,12 @@
 
 module.exports = function (cssContent) {
-    return cssContent.replace(/\.[\w\-]+\s\{/g, function (str) {
-        return str.replace(/\-/g, '_')
-    });
+    return cssContent
+        .replace(/\.[\w\-]+\s\{/g, function (str) {
+            var className = str.slice(1, str.length - 2);
+            var classNameConverted = className
+                .replace(/\b\w/g, function(l){ return l.toUpperCase() })
+                .replace(/\-/g, '');
+            return "export const " + classNameConverted + " = styled.span`";
+        })
+        .replace(/\}/g, "`");
 };
--- converter_spec.js
+++ converter_spec.js
@@ -3,7 +3,7 @@ var converter = require('./converter');
 
 describe('converter', function () {
 
-    it('simple', function () {
+    it('one class', function () {
         var css = ".heading-h-1-left {\n" +
             "\tfont-family: BloggerSans;\n" +
             "\tfont-size: 64px;\n" +
@@ -20,6 +20,47 @@ describe('converter', function () {
             "\tline-height: 1.09;\n" +
             "\ttext-align: left;\n" +
             "\tcolor: #525252;\n" +
+            "`";
+
+        expect(converter(css)).to.equal(styledComponent);
+
+    });
+
+    it('two classes', function () {
+        var css = ".heading-h-1-left {\n" +
+            "\tfont-family: BloggerSans;\n" +
+            "\tfont-size: 64px;\n" +
+            "\tfont-weight: 500;\n" +
+            "\tline-height: 1.09;\n" +
+            "\ttext-align: left;\n" +
+            "\tcolor: #525252;\n" +
+            "}\n" +
+            "\n" +
+            ".heading-h-1-left-light {\n" +
+            "\tfont-family: BloggerSans;\n" +
+            "\tfont-size: 64px;\n" +
+            "\tfont-weight: 500;\n" +
+            "\tline-height: 1.09;\n" +
+            "\ttext-align: left;\n" +
+            "\tcolor: #ffffff;\n" +
+            "}";
+
+        var styledComponent = "export const HeadingH1Left = styled.span`\n" +
+            "\tfont-family: BloggerSans;\n" +
+            "\tfont-size: 64px;\n" +
+            "\tfont-weight: 500;\n" +
+            "\tline-height: 1.09;\n" +
+            "\ttext-align: left;\n" +
+            "\tcolor: #525252;\n" +
+            "`\n" +
+            "\n" +
+            "export const HeadingH1LeftLight = styled.span`\n" +
+            "\tfont-family: BloggerSans;\n" +
+            "\tfont-size: 64px;\n" +
+            "\tfont-weight: 500;\n" +
+            "\tline-height: 1.09;\n" +
+            "\ttext-align: left;\n" +
+            "\tcolor: #ffffff;\n" +
             "`";
 
         expect(converter(css)).to.equal(styledComponent);
